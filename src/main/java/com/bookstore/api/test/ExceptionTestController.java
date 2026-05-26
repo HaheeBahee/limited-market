f1c7ba44e10e83ba4e268ed19d7c77adfb84822d
HaheeBahee <parkadg@naver.com>
2026-05-26 21:55:09 +0900

    test(global): 예외 처리 동작 확인용

---
+++ src/main/java/com/bookstore/api/test/ExceptionTestController.java
@@ -0,0 +1,27 @@
+package com.bookstore.api.test;
+
+import com.bookstore.api.global.exception.member.DuplicateEmailException;
+import com.bookstore.api.global.response.ApiResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/test")
+public class ExceptionTestController {
+
+    @GetMapping("/error")
+    public ResponseEntity<ApiResponse<Void>> testError() {
+        throw new DuplicateEmailException();
+    }
+
+    @GetMapping("/success")
+    public ResponseEntity<ApiResponse<String>> testSuccess() {
+        return ResponseEntity.ok(ApiResponse.success("테스트 데이터", "성공했습니다"));
+    }
+
+}
